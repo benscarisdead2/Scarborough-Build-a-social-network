0f5519426c9e4d2e0e4aabf9d4ee9e329d1272e0
ben scarborough <benscarborough@scarflix.com>
2017-07-01 21:34:57 -0400

    did some cool shit

--- app.js
+++ app.js
@@ -2,43 +2,36 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const session = require("express-session");
+
 const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
 const morgan = require("morgan");
 const routes = require("./routes/router.js");
 const models = require("./models")
-
+const session = require("express-session");
 //start app
 const app = express();
 
-const john = models.users.build({
-    name: 'john',
-    password: 'password'
-})
-
-john.save().then(function (newUser) {
-    console.log(newUser)
-})
-
-
 app.use(session({
-    secret: 'keyboard cat',
+    secret: 'jumpitoff',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false
 }))
+
 
 app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache')
 // app.set('layout', 'layout');
-app.use('/static', express.static('static'));
+// app.use('/static', express.static('static'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(morgan('dev'))
 
 app.use(routes);
+
+
 
 app.listen(3000, function () {
     console.log('Express running on http://localhost:3000/.')
--- routes/router.js
+++ routes/router.js
@@ -2,27 +2,51 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+// define an authentication routine
+function guard(req, res){
+    // if we have a user logged in...
+    if (req.session.user) {
+        // ... pass on to the next function
+        next();
+    } else {
+        // squawk loudly!
+        var err = new Error("Not logged in!");
+        next(err);
+    }
+};
 
+// temp storage for user information
+var Users = [];
 
+// define a route handler for /login
+router.get('/login', function (req, res) {
+    res.render('login');
+});
 
-router.get('/', function (req, res){
-    res.render('login')
-})
+router.get('/home', function (req, res) {
+    res.render('home');
+});
 
-router.get('/signup', function (req, res){
-    res.render('signup')
-})
+router.get('/create', guard, function (req, res) {
+    res.render('create');
+});
 
-router.get('/login', function (req, res){
-    res.render('login')
-})
+// respond to POST request from input form
+router.post('/login', function (req, res) {
+    console.log([req.body.username,req.body.password]);
+    // if the fields are not both filled, balk
+    if (!req.body.username || !req.body.password) {
+        res.status("400");
+        res.send("Invalid details!");
+    // if both fields are filled, add new user to the array and redirect   
+    } else {
+        var newUser = { usr: req.body.username, pwd: req.body.password };
+        Users.push(newUser);
+        console.log(Users);
+        req.session.user = newUser;
+        res.redirect('home');
+    }
+});
 
-router.get('/create', function (req, res){
-    res.render('create')
-})
-
-router.get('/home', function (req, res){
-    res.render('home')
-})
 
 module.exports = router;
